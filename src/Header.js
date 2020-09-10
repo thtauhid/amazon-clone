cfe5c7bfbb8479f767d581d7b2b6ec155c96b66b
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-09-11 02:01:06 +0600

    init

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            
+            <div className="header__search">
+                 
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByQIkSRQTbwSQnln0x1j0cGWe2mYNEVpE",
+    authDomain: "clone-xyz.firebaseapp.com",
+    databaseURL: "https://clone-xyz.firebaseio.com",
+    projectId: "clone-xyz",
+    storageBucket: "clone-xyz.appspot.com",
+    messagingSenderId: "352884695262",
+    appId: "1:352884695262:web:cb041292628919e5aaa24f",
+    measurementId: "G-1DJWJ4X3W7"
+  };
