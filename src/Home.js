62be6f9d56f11e19dedb864f8b4f60043719bb13
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-09-11 22:44:51 +0600

    progress

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg"
+      />
+      <div className="home__row">
+        {/* Product */}
+        {/* Product */}
+      </div>
+
+      <div className="home__row">
+        {/* Product */}
+        {/* Product */}
+        {/* Product */}
+      </div>
+
+      <div className="home__row">{/* Product */}</div>
+    </div>
+  );
+}
+
+export default Home;
